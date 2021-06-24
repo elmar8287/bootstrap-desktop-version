d92364b34c7378739ee47b83d47e0b5ad6fc0b7c
elmar8287 <L-mar@inbox.ru>
2021-06-24 17:53:33 +0400

    Fix JS linter issues

--- index.js
+++ index.js
@@ -311,8 +311,8 @@ setInterval(() => {
 // if the email is not match to LowerCase then the alert display
 
 function validateForm() {
-  let x = document.forms["myForm"]["user_email"].value;
-  var mailchecker = x.toLowerCase();
+  const x = document.forms['myForm']['user_email'].value;
+  let mailchecker = x.toLowerCase();
   if (!x.match(mailchecker)) {
     alert("Email should be a lower case letters");
     return false;
